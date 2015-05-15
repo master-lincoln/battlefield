ea8d33cdafc8f9beb5728b3eb4970aea7d23fae0
lukaszlipinski <lukasz.lipinski@innogames.de>
2015-05-15 10:21:21 +0200

    Refactorization to require.js

---
+++ js/views/battlefield.js
@@ -0,0 +1,11 @@
+define('view/battlefield', ['backbone'], function(Backbone) {
+	return Backbone.View.extend({
+		initialize : function() {
+			this.render();
+		},
+
+		render : function() {
+
+		}
+	});
+});
