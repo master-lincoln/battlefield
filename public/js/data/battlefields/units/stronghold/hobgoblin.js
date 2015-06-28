df314956c4c05b54a5c9e28f853f80239824c88e
Lukasz Lipinski <uzza17@gmail.com>
2015-06-28 18:27:57 +0200

    removing unused properties

--- public/js/data/battlefields/units/stronghold/hobgoblin.js
+++ public/js/data/battlefields/units/stronghold/hobgoblin.js
@@ -7,93 +7,63 @@ define('data/battlefields/units/stronghold/hobgoblin', [
 		var obj = {};
 
 		obj[battlefieldUnitAnimationTypesEnum.MOVING] = {
-			steps : [10, 11, 12, 13, 14, 15, 16, 17, 18],
-			repeat : Infinity,
-			movement : true
+			steps : [10, 11, 12, 13, 14, 15, 16, 17, 18]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.MOUSEOVER] = {
-			steps : [1, 5, 6, 7, 8, 8, 7, 6, 5],
-			repeat : Infinity,
-			movement : false
+			steps : [1, 5, 6, 7, 8, 8, 7, 6, 5]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.STANDING] = {
-			steps : [1, 2, 3, 4, 3, 2, 1],
-			repeat : Infinity,
-			movement : false
+			steps : [1, 2, 3, 4, 3, 2, 1]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.MOUSEOVER_ACTIVE] = {
-			steps : [60, 64, 65, 66, 67, 67, 66, 65, 64],
-			repeat : Infinity,
-			movement : false
+			steps : [60, 64, 65, 66, 67, 67, 66, 65, 64]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.STANDING_ACTIVE] = {
-			steps : [60, 61, 62, 63, 62, 61, 60],
-			repeat : Infinity,
-			movement : false
+			steps : [60, 61, 62, 63, 62, 61, 60]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.HIT] = {
-			steps : [1, 46, 47, 48, 49, 51],
-			repeat : 1,
-			movement : false
+			steps : [1, 46, 47, 48, 49, 51]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.DEFEND] = {
-			steps : [1, 22, 23, 24, 25, 26, 27, 28],
-			repeat : 1,
-			movement : false
+			steps : [1, 22, 23, 24, 25, 26, 27, 28]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.DEATH] = {
-			steps : [1, 52, 53, 54, 55, 56, 57, 58, 59],
-			repeat : 1,
-			movement : false
+			steps : [1, 52, 53, 54, 55, 56, 57, 58, 59]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.TURN_LEFT] = {
-			steps : [1, 20, 21],
-			repeat : 1,
-			movement : false
+			steps : [1, 20, 21]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.TURN_RIGHT] = {
-			steps : [21, 20, 1],
-			repeat : 1,
-			movement : false
+			steps : [21, 20, 1]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.ATTACK_UP] = {
-			steps : [1, 34, 35, 29, 30, 31, 32, 33],
-			repeat : 1,
-			movement : false
+			steps : [1, 34, 35, 29, 30, 31, 32, 33]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.ATTACK_STRAIGHT] = {
-			steps : [1, 34, 35, 36, 37, 38, 39, 40],
-			repeat : 1,
-			movement : false
+			steps : [1, 34, 35, 36, 37, 38, 39, 40]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.ATTACK_DOWN] = {
-			steps : [1, 34, 35, 41, 42, 43, 44, 45],
-			repeat : 1,
-			movement : false
+			steps : [1, 34, 35, 41, 42, 43, 44, 45]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.START_MOVING] = {
-			steps : [9],
-			repeat : 1,
-			movement : false
+			steps : [9]
 		};
 
 		obj[battlefieldUnitAnimationTypesEnum.STOP_MOVING] = {
-			steps : [19],
-			repeat : 1,
-			movement : false
+			steps : [19]
 		};
 
 		return obj;
